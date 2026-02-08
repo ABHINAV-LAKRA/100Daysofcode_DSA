ac34bb0f8f2bbe3b5ddaafd97d9b79b5824d1883
ABHINAV-LAKRA <223014805+ABHINAV-LAKRA@users.noreply.github.com>
2026-02-08 23:19:37 +0530

    Add files via upload

---
+++ day8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    else
+        return a * power(a, b - 1);
+}
+
+int main()
+{
+    int a, b;
+
+    printf("Enter base (a): ");
+    scanf("%d", &a);
+
+    printf("Enter power (b): ");
+    scanf("%d", &b);
+
+    printf("%d^%d = %d\n", a, b, power(a, b));
+
+    return 0;
+}
