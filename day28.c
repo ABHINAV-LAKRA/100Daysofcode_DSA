d56dbcde2b2e5e3b48782f7d668781ce0011dbb9
ABHINAV-LAKRA <223014805+ABHINAV-LAKRA@users.noreply.github.com>
2026-02-28 23:38:06 +0530

    Add files via upload

---
+++ day28.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* create(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int value;
+
+    if (n <= 0)
+        return NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+
+        if (head == NULL) {
+            head = newNode;
+            newNode->next = head;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            newNode->next = head;
+            temp = newNode;
+        }
+    }
+
+    return head;
+}
+
+void display(struct Node* head) {
+    if (head == NULL)
+        return;
+
+    struct Node* temp = head;
+
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = create(n);
+    display(head);
+
+    return 0;
+}
