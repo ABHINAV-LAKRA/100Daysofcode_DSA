4c588aee7adf265945cd3083be786c77c711ca50
Aryan Yadav <223014805+ABHINAV-LAKRA@users.noreply.github.com>
2026-03-27 23:04:31 +0530

    Add files via upload

---
+++ day56.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+void rightView(struct Node* root) {
+    if (root == NULL) return;
+
+    struct Node* queue[1000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+            struct Node* curr = queue[front++];
+
+            if (i == size - 1) {
+                printf("%d ", curr->data);
+            }
+
+            if (curr->left != NULL)
+                queue[rear++] = curr->left;
+
+            if (curr->right != NULL)
+                queue[rear++] = curr->right;
+        }
+    }
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->right = createNode(5);
+    root->right->right = createNode(4);
+
+    printf("Right View: ");
+    rightView(root);
+
+    return 0;
+}
